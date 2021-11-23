8a9370c1404c0b065b745703f7072a85caa6720b
Rociobohn <rocioquinterosbohn@gmail.com>
2021-11-23 12:47:17 -0300

    mi commit

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 
 app.use(express.static('public'))
 
-app.listen(3000, () => {
-    console.log('Servidor Funcionando');
-
+app.listen (process.env.PORT ||3000, ()=>{
+    console.log('Servidor funcionando bien');
 });
+
 console.log(__dirname);
 
 app.get('/', (req, res) => {
